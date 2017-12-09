a649733cd318a81e7d6a013b138baf1ade68504e
alyalqarni <30639922+alyalqarni@users.noreply.github.com>
2017-12-08 22:50:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include "ofApp.h"
+#include "ofAppGlutWindow.h"
+
+//--------------------------------------------------------------
+int main(){
+    ofAppGlutWindow window; // create a window
+    // set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+    ofSetupOpenGL(&window, 800, 700, OF_FULLSCREEN);
+    ofRunApp(new ofApp()); // start the app
+}
 
